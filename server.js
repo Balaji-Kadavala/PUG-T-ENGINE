96eb6249689f1c2fccc0ceabc6fbec116246ed72
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-17 13:58:12 +0530

    add API for external request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var fs = require("fs");
+var cors = require("cors");
+app.use(cors());
 app.set("view engine", "pug");
 
 app.get("/home/:fullname", (req, res) => {
@@ -16,6 +18,15 @@ app.get("/movies{/:startingIdx}{/:perPage}", (req, res) => {
   //console.log("fd:",JSON.parse(fd).slice(startingIdx, startingIdx + perPage))
   res.render("movies", { movies, startingIdx, perPage });
   // res.send(movies);
+});
+
+
+//This API is for fetching movies data from the other project
+app.get("/movies-data", (req, res) => {
+  var fd = fs.readFileSync("movies.json").toString();
+  var movies = JSON.parse(fd);
+  console.log("Request received for movies data");
+  res.send({movies});
 });
 
 app.listen(3600, () => {
