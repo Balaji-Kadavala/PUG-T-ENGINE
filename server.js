a4e7102f288a918abb77ccda9d92f7af853b2b2e
BALAJI KADAVALA <balaji.kadavala@gmail.com>
2026-04-17 12:08:49 +0530

    cookies setup register & login system

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+app.set("view engine", "pug");
+
+app.get("/home/:fullname", (req, res) => {
+  res.render("index", { fullname: req.params.fullname });
+});
+
+
+app.get("/movies{/:startingIdx}{/:perPage}", (req, res) => {
+  var startingIdx = +req.params.startingIdx || 0;
+  var perPage = +req.params.perPage || 10;
+  var fd = fs.readFileSync("movies.json").toString();
+  var movies = JSON.parse(fd).slice(startingIdx, startingIdx + perPage);
+  console.log("fd:",JSON.parse(fd).slice(startingIdx, startingIdx + perPage))
+  res.render("movies", { movies, startingIdx, perPage });
+  // res.send(movies);
+});
+
+app.listen(3600, () => {
+  console.log("server 3600 port lo vintundi");
+});
